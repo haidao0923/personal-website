cce6d803530c5ea14bb595edefd48d991e5b26f5
hdao35 <hdao35@gatech.edu>
2023-12-26 19:25:37 -0500

    prevent submitting if empty message field

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -3,8 +3,26 @@ import StarRating from './StarRating';
 import emailjs from '@emailjs/browser';
 
 const ContactForm: React.FC = () => {
-  const [rating, setRating] = useState(5);
+  const [email, setEmail] = useState("noemail@gmail.com");
+  const [name, setName] = useState("Your Secret Admirer");
+  const [message, setMessage] = useState("Write something...");
+  const [rating, setRating] = useState(0);
 
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(`Email: ${event.target.value}`);
+    setEmail(event.target.value);
+  }
+
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(`Name: ${event.target.value}`);
+    setName(event.target.value);
+  }
+
+  const handleMessageChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    console.log(`Message: ${event.target.value}`);
+    setMessage(event.target.value);
+  }
 
   const handleRatingChange = (rating: number) => {
     console.log(`Selected rating: ${rating}`);
@@ -15,23 +33,28 @@ const ContactForm: React.FC = () => {
     e.preventDefault();
 
     emailjs.sendForm('service_atdncm5', 'template_i7ko30h', e.currentTarget, 'Wj1wGSlJqR_HvHw2U');
+
     console.log("Email Sent");
+    console.log(`Data Sent:\nEmail: ${email}\nName: ${name}\nMessage: ${message}\nRating ${rating}`)
 
   }
 
   return (
     <form className='form' onSubmit={sendEmail}>
         <h1>Email</h1>
-        <input id="email" className="email" name="email_from" placeholder="Email"></input>
+        <input id="email" className="email" placeholder="Email"  onChange={handleEmailChange}></input>
         <h1>Name</h1>
-        <input id="name" className="name" name="name_from" placeholder="Your Secret Admirer"></input>
+        <input id="name" className="name" placeholder="Your Secret Admirer" onChange={handleNameChange}></input>
         <h1>Message</h1>
-        <textarea id="message" className="message" name="message" placeholder="Write something..." rows={4}></textarea>
+        <textarea id="message" className="message" placeholder="Write something..." rows={4} onChange={handleMessageChange}></textarea>
         <br></br>
         <StarRating onChange={handleRatingChange}></StarRating>
         <br></br>
+        <input type="hidden" name="email_from" value={email}></input>
+        <input type="hidden" name="name_from" value={name}></input>
+        <input type="hidden" name="message" value={message}></input>
         <input type="hidden" name="rating_form" value={rating}></input>
-        <button type="submit" className='submit'>Submit</button>
+        <button type="submit" className='submit' disabled={message.trim().length > 0 ? false: true}>Submit</button>
         <br></br>
     </form>
   );
